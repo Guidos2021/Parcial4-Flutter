16596e8fb1156e0d681c6101a41372ae6d4d0972
Guidos2021 <Gilbertovela2015@gmail.com>
2023-05-20 17:42:05 -0600

    Actualizacion 1

--- lib/services/firebase.dart
+++ lib/services/firebase.dart
@@ -22,16 +22,16 @@ Future<void> agregarCliente(String usuario) async {
   await baseD.collection('clientes').add({"ciudad": usuario});
 }
 
-Future<void> agregarVehiculo(String usuario) async {
-  await baseD.collection('vehiculo').add({"marca": usuario});
-  await baseD.collection('vehiculo').add({"modelo": usuario});
-}
-
 Future<void> ActualizarCliente(String usuario) async {
   await baseD.collection('clientes').add({"nombres": usuario});
   await baseD.collection('clientes').add({"apellidos": usuario});
   await baseD.collection('clientes').add({"direccion": usuario});
   await baseD.collection('clientes').add({"ciudad": usuario});
+}
+
+Future<void> agregarVehiculo(String usuario) async {
+  await baseD.collection('vehiculo').add({"marca": usuario});
+  await baseD.collection('vehiculo').add({"modelo": usuario});
 }
 
 Future<void> ActuzalizarVehiculo(String usuario) async {
